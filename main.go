07a0defe95252167d0d895aefa8ddf8b4b92ab41
Corey Robinson <coreyrob52@gmail.com>
2025-06-01 12:38:37 -0500

    chore: code clean up

--- main.go
+++ main.go
@@ -17,15 +17,15 @@ import (
 )
 
 const (
-	screenWidth = 128
+	screenWidth  = 128
 	screenHeight = 128
-	playerSize = 8
-	sampleRate = 48000
-	volume = .05
+	playerSize   = 8
+	sampleRate   = 48000
+	volume       = .05
 )
 
 type Stream struct {
-	pos int64
+	pos  int64
 	freq float64
 }
 
@@ -33,10 +33,9 @@ func (self Stream) Read(buf []byte) (int, error) {
 	const bytesPerSample = 8
 	length := sampleRate / self.freq
 	n := len(buf) / bytesPerSample * bytesPerSample
-	
 
 	for i := 0; i < n/bytesPerSample; i++ {
-		v := math.Float32bits(float32(math.Sin(2 * math.Pi * float64(self.pos/bytesPerSample+int64(i))/length)))
+		v := math.Float32bits(float32(math.Sin(2 * math.Pi * float64(self.pos/bytesPerSample+int64(i)) / length)))
 		buf[8*i] = byte(v)
 		buf[8*i+1] = byte(v >> 8)
 		buf[8*i+2] = byte(v >> 16)
@@ -56,12 +55,12 @@ func (self *Stream) Close() error {
 	return nil
 }
 
-
 type PlayArea struct {
 	Top, Bottom, Left, Right float32
 }
 
 type Direction int
+
 const (
 	North Direction = iota
 	South
@@ -70,12 +69,17 @@ const (
 )
 
 func (self Direction) String() string {
-	switch (self) {
-	case North: return "North"
-	case South: return "South"
-	case East: return "East"
-	case West: return "West"
-	default: return ""
+	switch self {
+	case North:
+		return "North"
+	case South:
+		return "South"
+	case East:
+		return "East"
+	case West:
+		return "West"
+	default:
+		return ""
 	}
 }
 
@@ -84,8 +88,8 @@ type Player struct {
 }
 
 type BodyPart struct {
-	X, Y float32
-	Direction Direction
+	X, Y          float32
+	Direction     Direction
 	PrevDirection Direction
 }
 
@@ -95,25 +99,24 @@ type Food struct {
 
 type Game struct {
 	AudioContext *audio.Context
-	Player *Player
-	PlayArea *PlayArea
-	Food Food
-	Delay int
-	Score int
-	IsOver bool
-	HasFood bool
-	KeyPressed bool
+	Player       *Player
+	PlayArea     *PlayArea
+	Food         Food
+	Delay        int
+	Score        int
+	IsOver       bool
+	HasFood      bool
+	KeyPressed   bool
 }
 
-
 func (self *Game) Update() error {
-	if self.IsOver { 
-		return nil 
+	if self.IsOver {
+		return nil
 	}
-	if !ebiten.IsKeyPressed(ebiten.KeyA) && 
-	!ebiten.IsKeyPressed(ebiten.KeyS) && 
-	!ebiten.IsKeyPressed(ebiten.KeyD) &&
-	!ebiten.IsKeyPressed(ebiten.KeyW) {
+	if !ebiten.IsKeyPressed(ebiten.KeyA) &&
+		!ebiten.IsKeyPressed(ebiten.KeyS) &&
+		!ebiten.IsKeyPressed(ebiten.KeyD) &&
+		!ebiten.IsKeyPressed(ebiten.KeyW) {
 		self.KeyPressed = false
 	}
 	if !self.HasFood {
@@ -122,7 +125,7 @@ func (self *Game) Update() error {
 		if err != nil {
 			logger.Fatal(err)
 		}
-		audioPlayer.SetVolume(volume*1.3)
+		audioPlayer.SetVolume(volume * 1.3)
 		audioPlayer.Play()
 		time.Sleep(time.Millisecond * 80)
 		audioPlayer.Close()
@@ -132,44 +135,54 @@ func (self *Game) Update() error {
 		if err != nil {
 			logger.Fatal(err)
 		}
-		audioPlayer2.SetVolume(volume*1.3)
+		audioPlayer2.SetVolume(volume * 1.3)
 		audioPlayer2.Play()
 		time.Sleep(time.Millisecond * 80)
 		audioPlayer2.Close()
 		var cx, cy int
 		for {
-			loop:
-			cx = rand.Intn(int(self.PlayArea.Right-self.PlayArea.Left))
-			cy = rand.Intn(int(self.PlayArea.Bottom-self.PlayArea.Top))
+		loop:
+			cx = rand.Intn(int(self.PlayArea.Right - self.PlayArea.Left))
+			cy = rand.Intn(int(self.PlayArea.Bottom - self.PlayArea.Top))
 			for _, part := range self.Player.Body {
-				if part.X == float32(cx) && part.Y == float32(cy){ goto loop }
+				if part.X == float32(cx) && part.Y == float32(cy) {
+					goto loop
+				}
 			}
 			break
 		}
-		self.Food.X = float32(math.Floor(float64(float32(cx) + self.PlayArea.Left)/8)*8)
-		self.Food.Y = float32(math.Floor(float64(float32(cy) + self.PlayArea.Top)/8)*8)
+		self.Food.X = float32(math.Floor(float64(float32(cx)+self.PlayArea.Left)/8) * 8)
+		self.Food.Y = float32(math.Floor(float64(float32(cy)+self.PlayArea.Top)/8) * 8)
 		self.HasFood = true
 	}
-	if math.Abs(float64(self.Food.X - self.Player.Body[0].X)) < playerSize && 
-		math.Abs(float64(self.Food.Y - self.Player.Body[0].Y)) < playerSize {
+	if math.Abs(float64(self.Food.X-self.Player.Body[0].X)) < playerSize &&
+		math.Abs(float64(self.Food.Y-self.Player.Body[0].Y)) < playerSize {
 		self.HasFood = false
-		i := len(self.Player.Body)-1
+		i := len(self.Player.Body) - 1
 		x := self.Player.Body[i].X
 		y := self.Player.Body[i].Y
-		if self.Player.Body[i].Direction == North { y += playerSize }
-		if self.Player.Body[i].Direction == East { x -= playerSize }
-		if self.Player.Body[i].Direction == West { x += playerSize }
-		if self.Player.Body[i].Direction == South { y -= playerSize }
+		if self.Player.Body[i].Direction == North {
+			y += playerSize
+		}
+		if self.Player.Body[i].Direction == East {
+			x -= playerSize
+		}
+		if self.Player.Body[i].Direction == West {
+			x += playerSize
+		}
+		if self.Player.Body[i].Direction == South {
+			y -= playerSize
+		}
 		self.Player.Body = append(self.Player.Body, &BodyPart{
 			X: x, Y: y,
 			Direction: self.Player.Body[i].Direction,
 		})
 		self.Score++
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyA) && 
-	(self.Player.Body[0].Direction != East && self.Player.Body[0].Direction != East) &&
-	!self.KeyPressed { 
-		self.Player.Body[0].Direction = West 
+	if ebiten.IsKeyPressed(ebiten.KeyA) &&
+		(self.Player.Body[0].Direction != East && self.Player.Body[0].Direction != East) &&
+		!self.KeyPressed {
+		self.Player.Body[0].Direction = West
 		self.KeyPressed = true
 		// A4
 		audioPlayer, err := self.AudioContext.NewPlayer(&Stream{freq: 440})
@@ -180,11 +193,11 @@ func (self *Game) Update() error {
 		audioPlayer.Play()
 		time.Sleep(time.Millisecond * 80)
 		audioPlayer.Close()
-	} 
+	}
 	if ebiten.IsKeyPressed(ebiten.KeyS) &&
-	(self.Player.Body[0].Direction != South && self.Player.Body[0].Direction != North) &&
-	!self.KeyPressed { 
-		self.Player.Body[0].Direction = South 
+		(self.Player.Body[0].Direction != South && self.Player.Body[0].Direction != North) &&
+		!self.KeyPressed {
+		self.Player.Body[0].Direction = South
 		self.KeyPressed = true
 		// G4
 		audioPlayer, err := self.AudioContext.NewPlayer(&Stream{freq: 391.9954})
@@ -195,11 +208,11 @@ func (self *Game) Update() error {
 		audioPlayer.Play()
 		time.Sleep(time.Millisecond * 80)
 		audioPlayer.Close()
-	} 
-	if ebiten.IsKeyPressed(ebiten.KeyD) && 
-	(self.Player.Body[0].Direction != East && self.Player.Body[0].Direction != West) && 
-	!self.KeyPressed { 
-		self.Player.Body[0].Direction = East 
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyD) &&
+		(self.Player.Body[0].Direction != East && self.Player.Body[0].Direction != West) &&
+		!self.KeyPressed {
+		self.Player.Body[0].Direction = East
 		self.KeyPressed = true
 		// F#4
 		audioPlayer, err := self.AudioContext.NewPlayer(&Stream{freq: 369.9944})
@@ -210,11 +223,11 @@ func (self *Game) Update() error {
 		audioPlayer.Play()
 		time.Sleep(time.Millisecond * 80)
 		audioPlayer.Close()
-	} 
+	}
 	if ebiten.IsKeyPressed(ebiten.KeyW) &&
-	(self.Player.Body[0].Direction != South && self.Player.Body[0].Direction != North) &&
-	!self.KeyPressed { 
-		self.Player.Body[0].Direction = North 
+		(self.Player.Body[0].Direction != South && self.Player.Body[0].Direction != North) &&
+		!self.KeyPressed {
+		self.Player.Body[0].Direction = North
 		self.KeyPressed = true
 		// E4
 		audioPlayer, err := self.AudioContext.NewPlayer(&Stream{freq: 329.6276})
@@ -225,30 +238,46 @@ func (self *Game) Update() error {
 		audioPlayer.Play()
 		time.Sleep(time.Millisecond * 80)
 		audioPlayer.Close()
-	} 
+	}
 
 	self.Delay++
-	if self.Delay % (playerSize * 2) == 0 {
+	if self.Delay%(playerSize*2) == 0 {
 		for i := range self.Player.Body {
 			self.Player.Body[i].PrevDirection = self.Player.Body[i].Direction
 			if i > 0 {
 				self.Player.Body[i].Direction = self.Player.Body[i-1].PrevDirection
 				if self.Player.Body[0].X == self.Player.Body[i].X &&
-				self.Player.Body[0].Y == self.Player.Body[i].Y {
+					self.Player.Body[0].Y == self.Player.Body[i].Y {
 					self.IsOver = true
 					return nil
 				}
 			}
 
-			if self.Player.Body[i].PrevDirection == North { self.Player.Body[i].Y -= playerSize }
-			if self.Player.Body[i].PrevDirection == East { self.Player.Body[i].X += playerSize }
-			if self.Player.Body[i].PrevDirection == West { self.Player.Body[i].X -= playerSize }
-			if self.Player.Body[i].PrevDirection == South { self.Player.Body[i].Y += playerSize }
+			if self.Player.Body[i].PrevDirection == North {
+				self.Player.Body[i].Y -= playerSize
+			}
+			if self.Player.Body[i].PrevDirection == East {
+				self.Player.Body[i].X += playerSize
+			}
+			if self.Player.Body[i].PrevDirection == West {
+				self.Player.Body[i].X -= playerSize
+			}
+			if self.Player.Body[i].PrevDirection == South {
+				self.Player.Body[i].Y += playerSize
+			}
 
-			if self.Player.Body[i].X < self.PlayArea.Left { self.Player.Body[i].X = self.PlayArea.Right - playerSize }
-			if self.Player.Body[i].X + playerSize > self.PlayArea.Right { self.Player.Body[i].X = self.PlayArea.Left }
-			if self.Player.Body[i].Y < self.PlayArea.Top { self.Player.Body[i].Y = self.PlayArea.Bottom - playerSize }
-			if self.Player.Body[i].Y + playerSize > self.PlayArea.Bottom { self.Player.Body[i].Y = self.PlayArea.Top }
+			if self.Player.Body[i].X < self.PlayArea.Left {
+				self.Player.Body[i].X = self.PlayArea.Right - playerSize
+			}
+			if self.Player.Body[i].X+playerSize > self.PlayArea.Right {
+				self.Player.Body[i].X = self.PlayArea.Left
+			}
+			if self.Player.Body[i].Y < self.PlayArea.Top {
+				self.Player.Body[i].Y = self.PlayArea.Bottom - playerSize
+			}
+			if self.Player.Body[i].Y+playerSize > self.PlayArea.Bottom {
+				self.Player.Body[i].Y = self.PlayArea.Top
+			}
 		}
 	}
 
@@ -258,70 +287,70 @@ func (self *Game) Update() error {
 func (self *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.White)
 	textOp := &text.DrawOptions{}
-	textOp.GeoM.Translate(0,0)
+	textOp.GeoM.Translate(0, 0)
 	textOp.ColorScale.ScaleWithColor(color.Black)
 	source, _ := text.NewGoTextFaceSource(bytes.NewReader(fonts.MPlus1pRegular_ttf))
 	text.Draw(screen, fmt.Sprintf("score: %d", self.Score), &text.GoTextFace{
 		Source: source,
-		Size: 8,
+		Size:   8,
 	}, textOp)
 
 	vector.DrawFilledRect(
-		screen, 
-		self.PlayArea.Left, 
-		self.PlayArea.Top, 
-		self.PlayArea.Right-8, 
-		self.PlayArea.Bottom-8, 
+		screen,
+		self.PlayArea.Left,
+		self.PlayArea.Top,
+		self.PlayArea.Right-8,
+		self.PlayArea.Bottom-8,
 		color.Black,
 		false,
 	)
 	vector.DrawFilledCircle(screen, self.Food.X+4, self.Food.Y+4, 2, color.White, false)
 	for _, part := range self.Player.Body {
 		vector.DrawFilledRect(
-			screen, 
-			part.X, 
-			part.Y, 
-			playerSize, 
-			playerSize, 
+			screen,
+			part.X,
+			part.Y,
+			playerSize,
+			playerSize,
 			color.White,
 			false,
-			)
+		)
 	}
 	if self.IsOver {
 		textO := &text.DrawOptions{}
-		textO.GeoM.Translate(32,48)
+		textO.GeoM.Translate(32, 48)
 		textO.ColorScale.ScaleWithColor(color.White)
 		text.Draw(screen, "game over", &text.GoTextFace{
 			Source: source,
-			Size: 12,
+			Size:   12,
 		}, textO)
 	}
 }
 
 func (self *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	 return screenWidth, screenHeight
+	return screenWidth, screenHeight
 }
 
 func main() {
 	ebiten.SetWindowTitle("Game")
 	ebiten.SetWindowSize(1920, 1080)
 
-	game := &Game {
+	game := &Game{
 		AudioContext: audio.NewContext(sampleRate),
 		Player: &Player{
 			Body: []*BodyPart{&BodyPart{
 				X: 64,
-				Y:  64,
+				Y: 64,
 			}},
 		},
 		PlayArea: &PlayArea{
-			Left: 8,
-			Top: 8,
-			Right: screenWidth-8,
-			Bottom: screenHeight-8,
+			Left:   8,
+			Top:    8,
+			Right:  screenWidth - 8,
+			Bottom: screenHeight - 8,
 		},
 		HasFood: false,
-		IsOver: false,
+		IsOver:  false,
 	}
 
 	if err := ebiten.RunGame(game); err != nil {
